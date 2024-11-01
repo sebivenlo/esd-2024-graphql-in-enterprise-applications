9050317258e8021c1491e1fb8fe743301d73598b
Gabriele Lavinskaite <gabrielelavinskaite@gmail.com>
2024-11-01 16:45:35 +0100

    working dev container

--- Workshop/index.js
+++
@@ -1,8 +0,0 @@
-const cassandra = require('cassandra-driver');
-
-const cassandraClient = new cassandra.Client({
-  contactPoints: [process.env.CASSANDRA_CONTACT_POINTS || 'localhost'],
-  localDataCenter: 'datacenter1',
-  keyspace: process.env.CASSANDRA_KEYSPACE || 'workshop'
-});
-
