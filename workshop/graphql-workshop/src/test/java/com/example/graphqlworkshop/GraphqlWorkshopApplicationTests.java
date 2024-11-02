ed0d9df3b2b24b427d3e2d4d050890b00ec49e37
Gabriele Lavinskaite <gabrielelavinskaite@gmail.com>
2024-11-02 19:40:42 +0100

    without db

---
+++ workshop/graphql-workshop/src/test/java/com/example/graphqlworkshop/GraphqlWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
